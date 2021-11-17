cdd4b7aee1eac357028909459052602b7e5ca736
aredhu88 <94365328+aredhu88@users.noreply.github.com>
2021-11-17 11:06:39 +0530

    Create total_of_pt.vb

---
+++ total_of_pt.vb
@@ -0,0 +1,43 @@
+Public Sub test()
+
+
+
+Dim c As Integer
+
+
+
+c = 1
+
+Range("m2").Select
+
+For c = 1 To n
+Dim annualfiretax As Currency
+dim sumofpt as currency
+Dim sumofft As Currency
+Dim sumofint As Currency
+Dim i As Integer
+annualfiretax = ActiveCell.Offset(0, -2).Value
+sumofint = 0
+sumofft = 0
+sumofpt = 0
+
+i = 0
+  
+  For i = 1 To 11
+  If ActiveCell.Value = 0 Then
+    sumofft = sumofft + annualfiretax
+    sumofint = sumofint + ActiveCell.Offset(0, 2).Value
+    sumofpt = sumofpt + ActiveCell.Offset(0, 1).Value
+  Else
+  sumofft = annualfiretax
+  sumofint = ActiveCell.Offset(0, 2).Value
+  sumofpt = ActiveCell.Offset(0, 1).Value
+  End If
+  ActiveCell.Offset(0, 3).Select
+  Next
+  ActiveCell.Offset(0, 1).Value = sumofpt
+  ActiveCell.Offset(0, 2).Value = sumofft
+  ActiveCell.Offset(0, 3).Value = sumofint
+  ActiveCell.Offset(1, -33).Select
+  Next
+  End Sub
